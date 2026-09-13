82e17f86ea2c6694bed6e5dd60327db095bec587
Trinh Doanh <Doanh.T2516042@sis.hust.edu.vn>
2026-09-13 07:08:33 +0700

    Complete assignment 4.1

---
+++ Lab01/4.1/DeclaresVariable.java
@@ -0,0 +1,15 @@
+public class DeclaresVariable {
+    public static void main(String[] args) {
+        String title = "The Lion King";
+        double cost = 19.95;
+        int id=1;
+        boolean isAvailable=true;
+        System.out.println(title);
+        System.out.println(cost);
+        System.out.println(id);
+        System.out.println(isAvailable);
+
+        int roundedCost=(int) cost;
+        System.out.println(roundedCost);
+    }
+}
