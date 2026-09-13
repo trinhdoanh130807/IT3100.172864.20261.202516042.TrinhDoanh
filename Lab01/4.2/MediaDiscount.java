1fe3faa84369396300796b013074ee3d68efa799
Trinh Doanh <Doanh.T2516042@sis.hust.edu.vn>
2026-09-13 07:26:39 +0700

    Complete Assignment 4.2

---
+++ Lab01/4.2/MediaDiscount.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class MediaDiscount {
+    public static void main(String[] args) {
+        String strCost =JOptionPane.showInputDialog("Enter the cost of the media:");
+        Double cost = Double.parseDouble(strCost);
+        if(cost>20){
+            System.out.println("Discounted");
+        }
+        else{
+            System.out.println("Full price");
+        }
+    }
+}
