92592ee558464e4bbff5340b6acc3739192fc1b2
Trinh Doanh <Doanh.T2516042@sis.hust.edu.vn>
2026-09-13 09:03:17 +0700

    Complete assignment 5.1

---
+++ Lab01/directory/5.1/Caculator.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class Caculator {
+    public static void main(String[] args) {
+        String doublenums1 = JOptionPane.showInputDialog("Enter the first number:");
+        double nums1 = Double.parseDouble(doublenums1);
+        String doublenums2 = JOptionPane.showInputDialog("Enter the second number:");
+        double nums2 = Double.parseDouble(doublenums2);
+        double sum = nums1 + nums2;
+        double diff2v1 = nums2-nums1;
+        double diff1v2 = nums1-nums2;
+        double product = nums1*nums2;
+        double quotient1v2 = nums1/nums2;
+        double quotient2v1 = nums2/nums1;
+        System.out.println("Summary: "+sum);
+        System.out.println("Difference 2v1: "+diff2v1);
+        System.out.println("Difference 1v2: "+diff1v2);
+        System.out.println("Product: "+product);
+        System.out.println("Quotient 1v2: "+quotient1v2);
+        System.out.println("Quotient 2v1: "+quotient2v1);
+    }
+}
